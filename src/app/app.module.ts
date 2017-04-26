0d9337b78b07cfca76b07a48e36e479fb9ce66a6
troy <troy.hu@okchem.com>
2017-04-26 11:43:49 +0800

    路由

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,23 +7,26 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {RouterModule, Routes} from '@angular/router';
 import {AboutComponent} from './about/about.component';
+import {AboutModule} from './about/about.module';
+// import {AboutComponent} from './about/about.component';
 
 const rootRoutes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
-  {path: 'about', component: AboutComponent}
+  // {path: 'about', component: AboutComponent}
+  {path: 'about', component: AboutComponent, loadChildren: 'AboutModule'}
 ];
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    AboutComponent
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     HttpModule,
+    AboutModule,
     RouterModule.forRoot(rootRoutes)
   ],
   providers: [],
